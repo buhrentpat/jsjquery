87e21279738dbacdb8b826d95144f9eac055763c
Brent Patching <brent.patching@gmail.com>
2017-04-06 11:49:54 +1000

    created grid, change color on hover

--- js/grid.js
+++ js/grid.js
@@ -1,31 +1,19 @@
-gridInt = 1
-rowMax = 16
-columnMax = 16
-
-i = 1
-
 $(document).ready(function(){
-	$('#container').prepend('<div class="square"></div>');
+	let squares = 1800,
+		WIDTH = 600;
+
 	startGrid();
+
+	function startGrid () {
+		for (var i = 0; i < squares; i++) {
+			for (var j = 0; i< squares; i++){
+				$('#container').prepend('<div class="square"></div>')
+			}
+			}
+		}
+
+	$('.square').hover(function(){
+		$(this).css("background-color", "blue")
+	})
 });
 
-function startGrid(){
-
-	count = $('.square').length;
-
-	loop = while (i < 16) {
-		$('#container').prepend('<div class="square"></div>')
-		i++
-	}
-
-};
-
-//count = $(xxxx).length;
-
-//we want a script that can create sixteen squares on one
-// line, recognise it's at the end and start a new line,
-// and recognise when it's gotten to sixteen squares
-
-//<div id="x"> </div>
-
-//$('#container > div').prepend('<div id="2"></div>)
