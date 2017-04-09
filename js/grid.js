bcb672dbac64b169db82473e16df1bef18ae8b05
Brent Patching <brent.patching@gmail.com>
2017-04-10 09:52:47 +1000

    added new background colors and hover colors

--- js/grid.js
+++ js/grid.js
@@ -12,8 +12,47 @@ $(document).ready(function(){
 			}
 		}
 
+//scripts that set default hover color and change hover color
+
 	$('.square').hover(function(){
 		$(this).css("background-color", "blue")
 	})
+
+	$('.blackh').click(function(){
+		$('.square').hover(function(){
+			$(this).css("background-color", "black")
+		})		
+	})
+	$('.yellowh').click(function(){
+		$('.square').hover(function(){
+			$(this).css("background-color", "yellow")
+		})		
+	})
+	$('.redh').click(function(){
+		$('.square').hover(function(){
+			$(this).css("background-color", "red")
+		})		
+	})
+	$('.blueh').click(function(){
+		$('.square').hover(function(){
+			$(this).css("background-color", "blue")
+		})		
+	})
+
+
+//scripts that change the background color
+
+	$('.blackb').click(function(){
+		$('.square').css("background-color", "black")
+	})
+	$('.yellowb').click(function(){
+		$('.square').css("background-color", "yellow")
+	})
+	$('.redb').click(function(){
+		$('.square').css("background-color", "red")
+	})
+	$('.blueb').click(function(){
+		$('.square').css("background-color", "blue")
+	})	
 });
 
